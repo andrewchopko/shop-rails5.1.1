62f45eef9c4228648e675903f5ffea4d8c3cd6a1
Andrew <andreychopko@gmail.com>
2017-06-05 13:37:49 +0300

    Little bugs fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :authorize
-	defore_action :set_i18n_locale_from_params
+	before_action :set_i18n_locale_from_params
   protect_from_forgery with: :exception
 
   protected
@@ -8,9 +8,10 @@ class ApplicationController < ActionController::Base
   def authorize
   	unless User.find_by(id: session[:user_id])
   		redirect_to login_url, notice: "Please Log In"
+    end
   end
 
-  def set_18n_locale_from_params
+  def set_i18n_locale_from_params
   	if params[:locale]
   		if I18n.available_locales.map(&:to_s).include?(params[:locale])
   			I18n.locale = params[:locale]
